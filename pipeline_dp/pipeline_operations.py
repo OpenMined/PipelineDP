47947f8eac32bac2d5aae3227feeced0152e0914
Zachery Ferreira <zachferr@google.com>
2021-05-04 11:59:22 -0400

    Add LocalPipelineOperations class and tests so that we have a common base to work from

--- pipeline_dp/pipeline_operations.py
+++ pipeline_dp/pipeline_operations.py
@@ -74,3 +74,33 @@ class BeamOperations(PipelineOperations):
 
   def count_per_element(self, col, stage_name: str):
     return col | stage_name >> combiners.Count.PerElement()
+
+class LocalPipelineOperations(PipelineOperations):
+  """Local Pipeline adapter."""
+  
+  def map(self, col, fn, stage_name: str):
+    pass
+
+  def map_tuple(self, col, fn, stage_name: str):
+    pass
+
+  def map_values(self, col, fn, stage_name: str):
+    pass
+
+  def group_by_key(self, col, stage_name: str):
+    pass
+
+  def filter(self, col, fn, stage_name: str):
+    pass
+
+  def keys(self, col, stage_name: str):
+    pass
+
+  def values(self, col, stage_name: str):
+    pass
+
+  def sample_fixed_per_key(self, col, n: int, stage_name: str):
+    pass
+
+  def count_per_element(self, col, stage_name: str):
+    pass
