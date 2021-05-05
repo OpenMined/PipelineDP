7f1e46f8499017da011d3fb13ecc738600344bbb
AlexandraImbrisca <alexandra.imbrisca6@gmail.com>
2021-05-05 12:30:18 +0300

    Add Budget accounting

--- pipeline_dp/budget_accounting.py
+++ pipeline_dp/budget_accounting.py
@@ -1,20 +1,91 @@
 """Privacy budget accounting for DP pipelines."""
 
 
-class BudgetAccountant:
-  """Manages privacy budget."""
+class Budget:
+  """Manages the budget allocated for an operation."""
 
-  def __init__(self, epsilon, delta):
-    """Construct a BudgetAccountant
+  def __init__(self, weight, use_eps, use_delta, eps=None, delta=None):
+    """Constructs a Budget.
+
+    The values for eps and delta will be computed only after the pipeline was
+    constructed and the method compute_budgets was called.
 
     Args:
-      epsilon, delta: parameters of (epsilon, delta)-differential privacy.
+      weight: The weight is used to get more accurate results.
+      use_eps: A boolean that is False when the operation doesn't need epsilon.
+      use_delta: A boolean that is False when the operation doesn't need delta.
     """
-    if epsilon <= 0:
-      raise ValueError(f"Epsilon must be positive, not {epsilon}")
-    if delta < 0:
-      raise ValueError(f"Delta must non-negative, not {delta}")
-    self._eps = epsilon
+    self._eps = eps
+    self._delta = delta
+    self.weight = weight
+    self.use_eps = use_eps
+    self.use_delta = use_delta
+
+  @property
+  def eps(self):
+    """eps: Optional; Parameter of (eps, delta)-differential privacy."""
+    if self._eps is None:
+      raise AssertionError("Privacy budget is not calculated yet.")
+    return self._eps
+
+  @property
+  def delta(self):
+    """delta: Optional; Parameter of (eps, delta)-differential privacy."""
+    if self._delta is None:
+      raise AssertionError("Privacy budget is not calculated yet.")
+    return self._delta
+
+  def set_eps_delta(self, eps, delta):
+    self._eps = eps
     self._delta = delta
 
-  # TODO: implement BudgetAccountant functionality.
+
+class BudgetAccountant:
+  """Manages the privacy budget."""
+
+  def __init__(self, epsilon, delta):
+    """Constructs a BudgetAccountant.
+
+    Args:
+      epsilon, delta: Parameters of (epsilon, delta)-differential privacy.
+    """
+    if epsilon <= 0:
+      raise ValueError(f"Epsilon must be positive, not {epsilon}.")
+    if delta < 0:
+      raise ValueError(f"Delta must be non-negative, not {delta}.")
+
+    self._eps = epsilon
+    self._delta = delta
+    self._requested_budgets = []
+
+  def request_budget(self, weight, use_eps, use_delta):
+    """Requests a budget.
+
+    Args:
+      weight: The weight used to compute epsilon and delta for the budget.
+      use_eps: A boolean that is False when the operation doesn't need epsilon.
+      use_delta: A boolean that is False when the operation doesn't need delta.
+
+    Returns:
+      A "lazy" budget object that doesn't contain epsilon/delta until the
+      method compute_budgets is called.
+    """
+    budget = Budget(weight=weight, use_eps=use_eps, use_delta=use_delta)
+    self._requested_budgets.append(budget)
+    return budget
+
+  def compute_budgets(self):
+    """Computes the budgets after constructing the pipeline."""
+    if not self._requested_budgets:
+      print("No budgets were requested.")
+      return
+
+    total_weight = sum([budget.weight for budget in self._requested_budgets])
+
+    for budget in self._requested_budgets:
+      eps = 0
+      delta = 0
+      if total_weight != 0:
+        eps = budget.use_eps * self._eps * budget.weight / total_weight
+        delta = budget.use_delta * self._delta * budget.weight / total_weight
+      budget.set_eps_delta(eps, delta)
--- tests/budget_accounting_test.py
+++ tests/budget_accounting_test.py
@@ -6,17 +6,37 @@ import pipeline_dp
 class BudgetAccountant(unittest.TestCase):
 
   def test_validation(self):
-    pipeline_dp.BudgetAccountant(epsilon=1, delta=1e-10)  # no exception
-    pipeline_dp.BudgetAccountant(epsilon=1, delta=0)  # no exception
+    pipeline_dp.BudgetAccountant(epsilon=1, delta=1e-10)  # No exception.
+    pipeline_dp.BudgetAccountant(epsilon=1, delta=0)  # No exception.
 
     with self.assertRaises(ValueError):
-      # epsilon must be positive
-      pipeline_dp.BudgetAccountant(epsilon=0, delta=1e-10)
+      pipeline_dp.BudgetAccountant(epsilon=0, delta=1e-10)  # Epsilon must be positive.
 
     with self.assertRaises(ValueError):
-      # epsilon must be positive
-      pipeline_dp.BudgetAccountant(
-          epsilon=0.5, delta=-1e-10)  # Delta must be non-negative
+      pipeline_dp.BudgetAccountant(epsilon=0.5, delta=-1e-10)  # Delta must be non-negative.
+
+  def test_request_budget(self):
+    budget_accountant = pipeline_dp.BudgetAccountant(epsilon=1, delta=0)
+    budget = budget_accountant.request_budget(1, False, False)
+    self.assertTrue(budget)  # An object must be returned.
+
+    with self.assertRaises(AssertionError):
+      print(budget.eps)  # The privacy budget is not calculated yet.
+
+    with self.assertRaises(AssertionError):
+      print(budget.delta)  # The privacy budget is not calculated yet.
+
+  def test_compute_budgets(self):
+    budget_accountant = pipeline_dp.BudgetAccountant(epsilon=1, delta=1e-6)
+    budget1 = budget_accountant.request_budget(1, True, False)
+    budget2 = budget_accountant.request_budget(3, True, True)
+    budget_accountant.compute_budgets()
+
+    self.assertEqual(budget1.eps, 0.25)
+    self.assertEqual(budget1.delta, 0)  # Delta should be 0 if use_delta is False.
+
+    self.assertEqual(budget2.eps, 0.75)
+    self.assertEqual(budget2.delta, 7.5e-07)
 
 
 if __name__ == '__main__':
