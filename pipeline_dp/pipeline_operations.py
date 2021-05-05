ded3387d8a4530b4aaffa43d1a61210a10b46668
Zachery Ferreira <zachferr@google.com>
2021-05-05 12:53:24 -0400

    Add map_tuple operation to pipeline_operations
    Also added relevant tests.
    As per the sync this week I updated the affected files to
    use 4 space indenting.

--- pipeline_dp/pipeline_operations.py
+++ pipeline_dp/pipeline_operations.py
@@ -6,101 +6,102 @@ import apache_beam.transforms.combiners as combiners
 
 
 class PipelineOperations(abc.ABC):
-  """Interface for pipeline frameworks adapters."""
+    """Interface for pipeline frameworks adapters."""
 
-  @abc.abstractmethod
-  def map(self, col, fn, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def map(self, col, fn, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def map_tuple(self, col, fn, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def map_tuple(self, col, fn, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def map_values(self, col, fn, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def map_values(self, col, fn, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def group_by_key(self, col, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def group_by_key(self, col, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def filter(self, col, fn, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def filter(self, col, fn, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def keys(self, col, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def keys(self, col, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def values(self, col, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def values(self, col, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def sample_fixed_per_key(self, col, n: int, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def sample_fixed_per_key(self, col, n: int, stage_name: str):
+        pass
 
-  @abc.abstractmethod
-  def count_per_element(self, col, stage_name: str):
-    pass
+    @abc.abstractmethod
+    def count_per_element(self, col, stage_name: str):
+        pass
 
 
 class BeamOperations(PipelineOperations):
-  """Apache Beam adapter."""
+    """Apache Beam adapter."""
 
-  def map(self, col, fn, stage_name: str):
-    return col | stage_name >> beam.Map(fn)
+    def map(self, col, fn, stage_name: str):
+        return col | stage_name >> beam.Map(fn)
 
-  def map_tuple(self, col, fn, stage_name: str):
-    return col | stage_name >> beam.MapTuple(fn)
+    def map_tuple(self, col, fn, stage_name: str):
+        return col | stage_name >> beam.MapTuple(fn)
 
-  def map_values(self, col, fn, stage_name: str):
-    return col | stage_name >> beam.MapTuple(lambda k, v: (k, fn(v)))
+    def map_values(self, col, fn, stage_name: str):
+        return col | stage_name >> beam.MapTuple(lambda k, v: (k, fn(v)))
 
-  def group_by_key(self, col, stage_name: str):
-    return col | stage_name >> beam.GroupByKey()
+    def group_by_key(self, col, stage_name: str):
+        return col | stage_name >> beam.GroupByKey()
 
-  def filter(self, col, fn, stage_name: str):
-    return col | stage_name >> beam.Filter(fn)
+    def filter(self, col, fn, stage_name: str):
+        return col | stage_name >> beam.Filter(fn)
 
-  def keys(self, col, stage_name: str):
-    return col | stage_name >> beam.Keys()
+    def keys(self, col, stage_name: str):
+        return col | stage_name >> beam.Keys()
 
-  def values(self, col, stage_name: str):
-    return col | stage_name >> beam.Values()
+    def values(self, col, stage_name: str):
+        return col | stage_name >> beam.Values()
 
-  def sample_fixed_per_key(self, col, n: int, stage_name: str):
-    return col | stage_name >> combiners.Sample.FixedSizePerKey(n)
+    def sample_fixed_per_key(self, col, n: int, stage_name: str):
+        return col | stage_name >> combiners.Sample.FixedSizePerKey(n)
 
-  def count_per_element(self, col, stage_name: str):
-    return col | stage_name >> combiners.Count.PerElement()
+    def count_per_element(self, col, stage_name: str):
+        return col | stage_name >> combiners.Count.PerElement()
+
 
 class LocalPipelineOperations(PipelineOperations):
-  """Local Pipeline adapter."""
-  
-  def map(self, col, fn, stage_name: str = None):
-    return map(fn, col)
+    """Local Pipeline adapter."""
 
-  def map_tuple(self, col, fn, stage_name: str):
-    pass
+    def map(self, col, fn, stage_name: str = None):
+        return map(fn, col)
 
-  def map_values(self, col, fn, stage_name: str):
-    pass
+    def map_tuple(self, col, fn, stage_name: str = None):
+        return [fn(k, v) for k, v in col]
 
-  def group_by_key(self, col, stage_name: str):
-    pass
+    def map_values(self, col, fn, stage_name: str):
+        pass
 
-  def filter(self, col, fn, stage_name: str):
-    pass
+    def group_by_key(self, col, stage_name: str):
+        pass
 
-  def keys(self, col, stage_name: str):
-    pass
+    def filter(self, col, fn, stage_name: str):
+        pass
 
-  def values(self, col, stage_name: str):
-    pass
+    def keys(self, col, stage_name: str):
+        pass
 
-  def sample_fixed_per_key(self, col, n: int, stage_name: str):
-    pass
+    def values(self, col, stage_name: str):
+        pass
 
-  def count_per_element(self, col, stage_name: str):
-    pass
+    def sample_fixed_per_key(self, col, n: int, stage_name: str):
+        pass
+
+    def count_per_element(self, col, stage_name: str):
+        pass
--- tests/pipeline_operations_test.py
+++ tests/pipeline_operations_test.py
@@ -4,22 +4,33 @@ from pipeline_dp.pipeline_operations import LocalPipelineOperations
 
 
 class PipelineOperationsTest(unittest.TestCase):
-  pass
+    pass
+
 
 class LocalPipelineOperationsTest(unittest.TestCase):
-  @classmethod
-  def setUpClass(cls):
-    cls.ops = LocalPipelineOperations()
+    @classmethod
+    def setUpClass(cls):
+        cls.ops = LocalPipelineOperations()
 
-  def test_local_map(self):
-    some_map = self.ops.map([1,2,3], lambda x: x)
-    # some_map is its own consumable iterator
-    self.assertIs(some_map, iter(some_map))
+    def test_local_map(self):
+        some_map = self.ops.map([1, 2, 3], lambda x: x)
+        # some_map is its own consumable iterator
+        self.assertIs(some_map, iter(some_map))
 
-    self.assertEqual(list(self.ops.map([1,2,3], str)),
-                     ["1", "2", "3"])
-    self.assertEqual(list(self.ops.map(range(5), lambda x: x ** 2)),
-                     [0, 1, 4, 9, 16])
+        self.assertEqual(list(self.ops.map([1, 2, 3], str)),
+                         ["1", "2", "3"])
+        self.assertEqual(list(self.ops.map(range(5), lambda x: x ** 2)),
+                         [0, 1, 4, 9, 16])
+
+    def test_local_map_tuple(self):
+        tuple_list = [(1, 2), (2, 3), (3, 4)]
+
+        self.assertEqual(self.ops.map_tuple(tuple_list, lambda k, v: k+v),
+                         [3, 5, 7])
+
+        self.assertEqual(self.ops.map_tuple(tuple_list, lambda k, v: (
+            str(k), str(v))), [("1", "2"), ("2", "3"), ("3", "4")])
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
