6c5ad708140500c75a8c414a4dec2f94a89bff2f
Łukasz Kożuchowski <Lukasz.Kozuchowski@gmail.com>
2021-05-22 19:06:34 +0200

    Add more local pipeline operations (#24)

--- pipeline_dp/pipeline_operations.py
+++ pipeline_dp/pipeline_operations.py
@@ -149,14 +149,14 @@ class SparkRDDOperations(PipelineOperations):
 class LocalPipelineOperations(PipelineOperations):
     """Local Pipeline adapter."""
 
-    def map(self, col, fn, stage_name: str = None):
+    def map(self, col, fn, stage_name: typing.Optional[str] = None):
         return map(fn, col)
 
     def map_tuple(self, col, fn, stage_name: typing.Optional[str] = None):
         return (fn(k, v) for k, v in col)
 
-    def map_values(self, col, fn, stage_name: str):
-        pass
+    def map_values(self, col, fn, stage_name: typing.Optional[str] = None):
+        return ((k, fn(v)) for k, v in col)
 
     def group_by_key(self, col, stage_name: typing.Optional[str] = None):
         def group_by_key_generator():
@@ -167,17 +167,17 @@ class LocalPipelineOperations(PipelineOperations):
                 yield item
         return group_by_key_generator()
 
-    def filter(self, col, fn, stage_name: str):
-        pass
+    def filter(self, col, fn, stage_name: typing.Optional[str] = None):
+        return filter(fn, col)
 
     def keys(self, col, stage_name: str):
         pass
 
-    def values(self, col, stage_name: str):
-        pass
+    def values(self, col, stage_name: typing.Optional[str] = None):
+        return (v for k, v in col)
 
     def sample_fixed_per_key(self, col, n: int, stage_name: str):
         pass
 
-    def count_per_element(self, col, stage_name: str):
-        pass
+    def count_per_element(self, col, stage_name: typing.Optional[str] = None):
+        yield from collections.Counter(col).items()
