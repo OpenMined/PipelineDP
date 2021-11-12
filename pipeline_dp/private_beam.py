39b764d8b77db71293642ba68166857f7653be8b
Preethi Raghavan <prerag@google.com>
2021-11-12 12:35:03 -0800

    merging PrivatePTransform Map and flatmap

--- pipeline_dp/private_beam.py
+++ pipeline_dp/private_beam.py
@@ -107,6 +107,7 @@ class Sum(PrivatePTransform):
 
         return dp_engine.aggregate(pcol, params, data_extractors)
 
+
 class Map(PrivatePTransform):
     """Transform class for performing Map on PrivatePCollection."""
 
