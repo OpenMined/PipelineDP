a6c3be665b4f6aba1b7a824262f1d1c412b3a70c
Preethi Raghavan <prerag@google.com>
2021-12-06 12:28:38 -0800

    Revert "conflict fix"
    This reverts commit 2fff615d798ddc17e208e467f4472caa8d066abf.

--- tests/private_beam_test.py
+++ tests/private_beam_test.py
@@ -1,7 +1,6 @@
 import unittest
 import apache_beam as beam
 from apache_beam.runners.portability import fn_api_runner
-<<<<<<< HEAD
 from apache_beam import pvalue
 from unittest.mock import patch
 import apache_beam.testing.util as beam_util
