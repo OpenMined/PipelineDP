21388a7541c0056cb58ae94007be03d5fdaf0426
Vadym Doroshenko <dvadym@google.com>
2022-01-14 20:34:35 +0100

    base combiner

--- pipeline_dp/combiners.py
+++ pipeline_dp/combiners.py
@@ -4,44 +4,23 @@ import abc
 class Combiner(abc.ABC):
     """Base class for all combiners.
 
-  Combiners are objects that encapsulate aggregations and computations of
-  differential private metrics. Combiners use accumulators to do aggregations.
-  Combiners contain logic, while accumulators contain data.
-  The API of combiners are inspired by Apache Beam CombineFn class.
-  """
+    Combiners are objects that encapsulate aggregations and computations of
+    differential private metrics. Combiners use accumulators to do aggregations.
+    Combiners contain logic, while accumulators contain data.
+    The API of combiners are inspired by Apache Beam CombineFn class.
+    """
 
     @abc.abstractmethod
-    def add_value(self, value):
-        """Adds 'value' to accumulate.
-    Args:
-      value: value to be added.
-
-    Returns: self.
-    """
-        pass
-
-    def _check_mergeable(self, accumulator: 'Accumulator'):
-        if not isinstance(accumulator, type(self)):
-            raise TypeError(
-                f"The accumulator to be added is not of the same type: "
-                f"{accumulator.__class__.__name__} != "
-                f"{self.__class__.__name__}")
-
-    @abc.abstractmethod
-    def add_accumulator(self, accumulator: 'Accumulator') -> 'Accumulator':
-        """Merges the accumulator to self and returns self.
-
-    Sub-class implementation is responsible for checking that types of
-    self and accumulator are the same.
-
-    Args:
-     accumulator:
-
-    Returns: self
-    """
+    def create_accumulator(self, values):
+        """Creates accumulator from 'values'."""
         pass
 
     @abc.abstractmethod
-    def compute_metrics(self):
+    def merge_accumulators(self, accumulator1, accumulator2):
+        """Merges the accumulators and returns accumulator."""
+        pass
+
+    @abc.abstractmethod
+    def compute_metrics(self, accumulator: 'Accumulator'):
         """Computes and returns the result of aggregation."""
         pass
---
+++ tests/combiners_test.py
@@ -0,0 +1,5 @@
+import pipeline_dp.combiners as combiners
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
