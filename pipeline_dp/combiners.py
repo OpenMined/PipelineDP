960f304300ed71a4aafc7d9c1e1e4c97051070d7
Vadym Doroshenko <dvadym@google.com>
2022-01-17 13:51:38 +0100

    comments updates

--- pipeline_dp/combiners.py
+++ pipeline_dp/combiners.py
@@ -5,9 +5,10 @@ class Combiner(abc.ABC):
     """Base class for all combiners.
 
     Combiners are objects that encapsulate aggregations and computations of
-    differential private metrics. Combiners use accumulators to do aggregations.
-    Combiners contain logic, while accumulators contain data.
+    differential private metrics. Combiners use accumulators to store the
+    aggregation state. Combiners contain logic, while accumulators contain data.
     The API of combiners are inspired by Apache Beam CombineFn class.
+    https://beam.apache.org/documentation/transforms/python/aggregation/combineperkey/#example-5-combining-with-a-combinefn
     """
 
     @abc.abstractmethod
