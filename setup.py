7da28b0037a1c3d18b7582ea2aac14a8d7bd9972
Vadym Doroshenko <dvadym@google.com>
2022-01-28 14:29:53 +0100

    beta

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(fname):
 
 setup_kwargs = {
     'name': 'pipeline-dp',
-    'version': '0.1.0-beta',
+    'version': '0.1.0',
     'description': 'Framework for applying differential privacy to large datasets using batch processing systems',
     'author': 'Chinmay Shah',
     'author_email': 'chinmayshah3899@gmail.com',
