38f205867f7d3e8841ea6457511db01205663d22
Vadym Doroshenko <dvadym@google.com>
2022-08-15 10:07:33 +0200

    addressed comments

--- utility_analysis_new/poisson_binomial.py
+++ utility_analysis_new/poisson_binomial.py
@@ -22,7 +22,7 @@ from typing import Sequence, Tuple
 
 
 def compute_pmf(probabilities: Sequence[float]) -> np.ndarray:
-    """Computes probability mass function of poisson binomial distribution."""
+    """Computes probability mass function of Poisson binomial distribution."""
     # Compute coefficients of Probability Generating Function (PGF), which
     # equals to
     # PGF(x) = \product_{p\in ps}(1-p + p*x)
@@ -35,21 +35,24 @@ def compute_pmf(probabilities: Sequence[float]) -> np.ndarray:
     return poisson_bin_probs
 
 
-def compute_exp_std_skewness(ps: Sequence[float]) -> Tuple[float, float, float]:
-    exp = np.sum(ps)
-    std = np.sqrt(np.sum([p * (1 - p) for p in ps]))
-    skewness = np.sum([p * (1 - p) * (1 - 2 * p) for p in ps]) / std**3
+def compute_exp_std_skewness(
+        probabilities: Sequence[float]) -> Tuple[float, float, float]:
+    exp = np.sum(probabilities)
+    std = np.sqrt(np.sum([p * (1 - p) for p in probabilities]))
+    skewness = np.sum([p * (1 - p) * (1 - 2 * p) for p in probabilities
+                      ]) / std**3
     return exp, std, skewness
 
 
 def compute_pmf_approximation(mean: float, sigma: float, skewness: float,
                               n: int):
-    """Computes approximate probability mass function of poisson binomial distribution.
+    """Computes approximate probability mass function of Poisson binomial distribution.
 
     The computation is based on paper chapter 3.3 (refined normal approximation)
     https://www.researchgate.net/publication/257017356_On_computing_the_distribution_function_for_the_Poisson_binomial_distribution
     """
     G = lambda x: (norm.cdf(x) + skewness * (1 - x * x) * norm.pdf(x) / 6)
-    xs = np.arange(-0.5, n + 1, 1)
-    cdf_values = G((xs - mean) / sigma)
+    xs = np.arange(-1, n + 1)
+    cdf_values = G((xs + 0.5 - mean) / sigma)
+    cdf_values = np.clip(cdf_values, 0, 1)
     return np.diff(cdf_values)
