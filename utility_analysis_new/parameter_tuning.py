66b7be14ef39cfd6245152d7db94aa940459b172
Vadym Doroshenko <dvadym@google.com>
2022-09-12 18:18:35 +0200

    parameter tuning API

--- utility_analysis_new/parameter_tuning.py
+++ utility_analysis_new/parameter_tuning.py
@@ -85,6 +85,7 @@ def _compute_frequency_histogram(
 
     return backend.map(col, sort_histogram, "Sort histogram")
 
+
 @dataclass
 class Histogram:
     """Represents a histogram over integers."""
@@ -97,6 +98,7 @@ class ContributionHistograms:
     """Histograms of privacy id contributions."""
     cross_partition_histogram: Histogram
     per_partition_histogram: Histogram
+
 
 @dataclass
 class UtilityAnalysisRun:
@@ -114,6 +116,7 @@ T = TypeVar('T')
 
 @dataclass
 class Bounds(Generic[T]):
+    """Defines restrictions for numbers (int or floats)."""
     lower: Optional[T] = None
     upper: Optional[T] = None
 
@@ -123,40 +126,91 @@ class Bounds(Generic[T]):
 
 
 @dataclass
-class ParametersToTune:  # only count for now, None means any value
-    noise_kind: Optional[pipeline_dp.NoiseKind] = None
+class TunedParameters:
+    """Contains parameters to tune.
+
+    Attributes of this class define restrictions on tuning on corresponding
+    attributes in pipeline_dp.AggregateParams.
+
+    Example:
+        max_partitions_contributed
+          1. = Bound(lower=1, upper=10) only numbers between 1 and 10 will be
+          considered during tuning for max_partitions_contributed.
+          2. = Bound(lower=None, upper=10) means numbers till 10.
+          3. = None means no restriction
+          4. = 3 means no tuning, max_partitions_contributed=3.
+    """
     max_partitions_contributed: Optional[Union[int, Bounds[int]]] = None
     max_contributions_per_partition: Optional[Union[int, Bounds[int]]] = None
+    min_sum_per_partition: Optional[Union[float, Bounds[float]]] = None
+    max_sum_per_partition: Optional[Union[float, Bounds[float]]] = None
 
-    # budget_weigh?
     def __post_init__(self):
-        if isinstance(self.max_partitions_contributed, int):
-            assert self.max_partitions_contributed > 0
-        elif isinstance(self.max_partitions_contributed, Bounds):
-            assert self.max_partitions_contributed.lower > 0
-        #todo extract verification to another
+
+        def check_int_attribute(value, name: str):
+            if value is None:
+                return
+            if isinstance(value, int):
+                if value <= 0:
+                    raise ValueError(f"{name} must be >0, but {name}={value}")
+            elif isinstance(value, Bounds):
+                if value.lower <= 0:
+                    raise ValueError(
+                        f"{name} lower bound must be >0, but {name}.lower={value}"
+                    )
+
+        check_int_attribute(self.max_partitions_contributed,
+                            "max_partitions_contributed")
+        check_int_attribute(self.max_contributions_per_partition,
+                            "max_contributions_per_partition")
 
 
 @dataclass
-class ParameterTuningOptions:  # maybe to split in Spec and Options
-    aggregation: pipeline_dp.Metrics
-    description: str
+class TuneOptions:
+    """Options for tuning process.
+
+    Note that parameters that are not tuned (e.g. metrics, noise kind) are taken
+    from aggregate_params.
+
+    Attributes:
+        epsilon, delta: differential private budget for aggregations for which
+        tuning is performed.
+        aggregate_params: parameters of aggregation.
+        function_to_minimize: which function of the error to minimize. In case
+        if this argument is callable, it should take 1 argument of type
+        AggregateErrorMetrics and return float.
+        parameters_to_tune: spefies which parameters are tunable and what
+        restrictions on their values.
+    """
+    epsilon: float
+    delta: float
+    aggregate_params: pipeline_dp.AggregateParams
     function_to_minimize: Union[MinimizingFunction, Callable]
-    parameters_to_tune: ParametersToTune
-    is_public_partitions: bool
+    parameters_to_tune: TunedParameters
 
 
 @dataclass
-class ParameterTuningResult:
-    recommended_params: List[pipeline_dp.AggregateParams]
+class TuneResult:
+    """Represents tune results.
+
+    Attributes:
+        recommended_params: recommended parameters to use, according to
+        minimizing function. Note, that those parameters might be not optimal,
+        since finding optimal parameters not always feasible.
+        options: input options of tuning (for convenience).
+        contribution_histograms: histograms of privacy id contributions.
+        utility_analysis_runs: the results of all utility analysis runs that
+        were performed during tuning process.
+    """
+    recommended_params: pipeline_dp.AggregateParams
+    options: TuneOptions
     contribution_histograms: ContributionHistograms
-    options: ParameterTuningOptions
     utility_analysis_runs: List[UtilityAnalysisRun]
 
 
 def perform_parameter_tuning(col,
-    contribution_histograms: ContributionHistograms,
-    options: ParameterTuningOptions,
-    data_extractors: pipeline_dp.DataExtractors,
-    public_partitions=None) -> ParameterTuningResult:
+                             contribution_histograms: ContributionHistograms,
+                             options: TuneOptions,
+                             data_extractors: pipeline_dp.DataExtractors,
+                             public_partitions=None) -> TuneResult:
     pass
