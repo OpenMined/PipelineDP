aaaa4623f8ce23a0edc1f866a43ca0c9acba6f80
Vadym Doroshenko <dvadym@google.com>
2022-09-27 16:25:50 +0200

    addressed commments

--- utility_analysis_new/parameter_tuning.py
+++ utility_analysis_new/parameter_tuning.py
@@ -318,6 +318,7 @@ class TuneResult:
 def _find_candidate_parameters(
     histograms: ContributionHistograms, parameters_to_tune: ParametersToTune
 ) -> utility_analysis_new.dp_engine.MultiParameterConfiguration:
+    """Uses some heuristics to find (hopefully) good enough parameters."""
     # TODO: decide where to put QUANTILES_TO_USE, maybe TuneOptions?
     QUANTILES_TO_USE = [0.9, 0.95, 0.98, 0.99, 0.995]
     l0_candidates = linf_candidates = None
@@ -355,15 +356,15 @@ def _find_candidate_parameters(
         max_contributions_per_partition=linf_bounds)
 
 
-def _utility_analysis_to_tune_result(
+def _convert_utility_analysis_to_tune_result(
         utility_analysis_result: Tuple, tune_options: TuneOptions,
         run_configurations: utility_analysis_new.dp_engine.
-    MultiParameterConfiguration, is_public_partitions: bool,
+    MultiParameterConfiguration, use_public_partitions: bool,
         contribution_histograms: ContributionHistograms) -> TuneResult:
 
     # Get only error metrics, ignore partition selection for now.
     # TODO: Make the output of the utility analysis 1 dataclass per 1 run.
-    if is_public_partitions:
+    if use_public_partitions:
         # utility_analysis_result contains only error metrics.
         aggregate_errors = utility_analysis_result
     else:
@@ -395,10 +396,22 @@ def tune(col,
          public_partitions=None) -> TuneResult:
     """Tunes parameters.
 
+    It works in the following way:
+        1. Based on quantiles of privacy id contributions, candidates for
+        contribution bounding parameters chosen.
+        2. Utility analysis run for those parameters.
+        3. The best parameter set is chosen according to
+          options.minimizing_function.
+
+    The result contains output metrics for all utility analysis which were
+    performed.
+
     Args:
         col: collection where all elements are of the same type.
           contribution_histograms:
         backend: PipelineBackend with which the utility analysis will be run.
+        contribution_histograms: contribution histograms that should be
+         computed with compute_contribution_histograms().
         options: options for tuning.
         data_extractors: functions that extract needed pieces of information
           from elements of 'col'.
@@ -421,17 +434,17 @@ def tune(col,
     utility_analysis_result = utility_analysis.perform_utility_analysis(
         col, backend, utility_analysis_options, data_extractors,
         public_partitions)
-    is_public_partitions = public_partitions is not None
+    use_public_partitions = public_partitions is not None
     return backend.map(
         utility_analysis_result,
-        lambda result: _utility_analysis_to_tune_result(
-            result, options, candidates, is_public_partitions,
+        lambda result: _convert_utility_analysis_to_tune_result(
+            result, options, candidates, use_public_partitions,
             contribution_histograms), "To Tune result")
 
 
 def _check_tune_args(options: TuneOptions):
     if options.aggregate_params.metrics != [pipeline_dp.Metrics.COUNT]:
-        raise NotImplementedError("Tuning only for count is supported.")
+        raise NotImplementedError("Tuning is supported only for Count.")
 
     if options.function_to_minimize != MinimizingFunction.ABSOLUTE_ERROR:
         raise NotImplementedError(
