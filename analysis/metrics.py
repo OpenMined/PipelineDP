7535e648697fa6362bd165a8ce6982b7baeaf821
Vadym Doroshenko <53558779+dvadym@users.noreply.github.com>
2023-02-24 10:59:35 +0100

    Refactor analysis output metrics: part 1 Adding new metrics (#400)

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -54,6 +54,7 @@ class AggregateMetricType(Enum):
     SUM = 'sum'
 
 
+# TODO(dvadym): Remove he following dataclasses, when the new ones are used.
 @dataclass
 class AggregateErrorMetrics:
     """Stores aggregate cross-partition metrics for utility analysis.
@@ -143,3 +144,159 @@ class AggregateMetrics:
     count_metrics: Optional[AggregateErrorMetrics] = None
     privacy_id_count_metrics: Optional[AggregateErrorMetrics] = None
     partition_selection_metrics: Optional[PartitionSelectionMetrics] = None
+
+
+# New dataclasses. They will be used as the output of Utility analysis in the
+# next PRs.
+
+
+@dataclass
+class MeanVariance:
+    mean: float
+    var: float
+
+
+@dataclass
+class ContributionBoundingErrors:
+    """Contains an error breakdown by types of contribution bounding.
+
+    Attributes:
+        l0: max_partition_contributed (aka l0) bounding error. The output of l0
+          bounding is a random variable for each partition. Its distribution is
+          close to normal when number of contribution per partition is large.
+        linf: per partition (aka linf) bounding error. The output of linf
+          bounding is deterministic for each partition.
+        linf_min & linf_max: represents error due to min & max contribution
+          bounding, respectively (only populated for Sum metrics). It is
+          deterministic for each partition.
+    """
+    l0: MeanVariance
+    linf: float
+    linf_min: float
+    linf_max: float
+
+
+@dataclass
+class ValueErrors:
+    """Errors between actual and dp metric.
+
+    This class describes breakdown of errors for (dp_value - actual_value),
+    where value can be a metric like count, sum etc. The value error is a random
+    variable and it comes from different sources - contribution bounding error
+    and DP noise. This class contains different error metrics.
+
+    All attributes correspond to the errors computed per partition and then
+    averaged across partitions, e.g.
+      rmse_per_partition = sqrt(E(dp_value - actual_value)^2)
+      self.rmse = mean(rmse_per_partition)
+
+    Attributes:
+        bounding_errors: contribution bounding errors.
+        bias: averaged across partitions E(dp_value - actual_value).
+        variance: averaged across partitions Var(dp_value - actual_value).
+        rmse: averaged across partitions sqrt(E(dp_value - actual_value)^2).
+        l1: averaged across partitions E|dp_value - actual_value|.
+        with_dropped_partitions: error which takes into consideration partitions
+          dropped due to partition selection. See example below.
+    """
+    bounding_errors: ContributionBoundingErrors
+    bias: float
+    variance: float
+    rmse: float
+    l1: float
+
+    # The following error metrics include error from dropped partitions.
+    #
+    # Consider the following example with a single partition to see how they are
+    # different from abs/rel_error_expected metrics:
+    #
+    # Given 1 partition with probability_to_keep=0.4, actual_count=100,
+    # abs_error_expected=-50;
+    # -> abs_error_expected = (0.4*-50)/0.4=-50
+    # -> abs_error_expected_w_dropped_partitions = 0.4*-50+0.6*-100=-80
+    #
+    # When public partitions are used, these will be exactly equal to
+    # abs/rel_error_expected.
+    with_dropped_partitions: float
+
+
+@dataclass
+class DataDropInfo:
+    """Information about the data dropped during different DP stages.
+
+    Attributes:
+        l0: ratio of data dropped during of l0 contribution bounding.
+        linf: ratio of data dropped during of linf contribution bounding.
+        partition_selection: ratio of data dropped because of partition
+          selection.
+
+    """
+    l0: float
+    linf: float
+
+    # This cannot be computed at PartitionSelectionUtility and needs to be
+    # computed for each aggregation separately, since it takes into account data
+    # drop from contribution bounding and that is aggregation-specific.
+    partition_selection: float
+
+
+@dataclass
+class MetricUtility:
+    """Stores aggregate cross-partition metrics for utility analysis.
+
+    Attributes:
+        metric: DP metric for which this analysis was performed.
+        num_dataset_partitions: the number of partitions in dataset.
+        num_non_public_partitions: the number of partitions dropped because
+          of public partitions.
+        num_empty_partitions: the number of empty partitions added because of
+          public partitions.
+        noise_std: the standard deviation of added noise.
+        noise_kind: the noise kind (Laplace or Gaussian)
+        ratio_data_dropped: the information about dropped data.
+        absolute_error: error in terms of (dp_value - actual_value).
+        relative_error: error in terms of (dp_value - actual_value)/actual_value.
+    """
+    metric: pipeline_dp.Metrics
+    num_dataset_partitions: int
+    num_non_public_partitions: int
+    num_empty_partitions: int
+
+    # Noise information.
+    noise_std: float
+    noise_kind: pipeline_dp.NoiseKind
+
+    # Dropped data breakdown.
+    ratio_data_dropped: DataDropInfo
+
+    # Value errors
+    absolute_error: ValueErrors
+    relative_error: ValueErrors
+
+
+@dataclass
+class PrivatePartitionSelectionUtility:
+    """Stores aggregate metrics about partition selection."""
+
+    strategy: pipeline_dp.PartitionSelectionStrategy
+    num_partitions: float
+    dropped_partitions: MeanVariance
+    ratio_dropped_data: float
+
+
+@dataclass
+class UtilityReport:
+    """Stores result of the utility analysis for specific input parameters.
+
+    Attributes:
+        input_aggregate_params: input parameters for which this utility analysis
+          was computed.
+        metric_errors: utility analysis of metrics (e.g. COUNT, SUM,
+          PRIVACY_ID_COUNT).
+        partition_selection_metrics: utility analysis of selected partition.
+    """
+    input_aggregate_params: pipeline_dp.AggregateParams
+
+    metric_errors: Optional[List[MetricUtility]] = None
+    partition_selection_metrics: Optional[
+        PrivatePartitionSelectionUtility] = None
