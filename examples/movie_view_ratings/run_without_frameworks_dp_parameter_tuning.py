1ee135b3bc992d2579a90fbf06c12ff9209840c6
RamSaw <mpravilov@google.com>
2023-03-29 16:03:33 +0200

    Implement run_without_frameworks_dp_parameter_tuning.py for movie_views_ratings.

--- examples/movie_view_ratings/run_without_frameworks_dp_parameter_tuning.py
+++ examples/movie_view_ratings/run_without_frameworks_dp_parameter_tuning.py
@@ -11,52 +11,35 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Demo of running PipelineDP locally, without any external data processing framework.
-Shows how to use compute the best contribution bounds
-(e.g. max_partitions_contributed) in a differential private way.
-
-1. Install Python and run on the command line `pip install pipeline-dp absl-py`
-2. Run python python run_without_frameworks_dp_parameter_tuning.py
---input_file=<path to data.txt from 3> --output_file=<...>
-"""
+""" Demo of running PipelineDP locally, without any external
+data processing framework"""
 
 from absl import app
 from absl import flags
 import pipeline_dp
-import pandas as pd
+
+from common_utils import parse_file, write_to_file
+from examples.movie_view_ratings import common_utils
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('input_file', 'restaurants_week_data.csv',
-                    'The file with the restaraunt visits data')
+flags.DEFINE_string('input_file', None, 'The file with the movie view data')
 flags.DEFINE_string('output_file', None, 'Output file')
-
-
-def write_to_file(col, filename):
-    with open(filename, 'w') as out:
-        out.write('\n'.join(sorted(map(str, col))))
 
 
 def main(unused_argv):
     # Load and parse input data
-    df = pd.read_csv(FLAGS.input_file)
-    df.rename(inplace=True,
-              columns={
-                  'VisitorId': 'user_id',
-                  'Time entered': 'enter_time',
-                  'Time spent (minutes)': 'spent_minutes',
-                  'Money spent (euros)': 'spent_money',
-                  'Day': 'day'
-              })
-    restaraunt_visits_rows = [index_row[1] for index_row in df.iterrows()]
+    movie_views = parse_file(FLAGS.input_file)
 
     # Specify how to extract privacy_id, partition_key and value from an
-    # element of restaraunt_visits_rows.
+    # element of movie_views.
     data_extractors = pipeline_dp.DataExtractors(
-        partition_extractor=lambda row: row.day,
-        privacy_id_extractor=lambda row: row.user_id,
-        value_extractor=lambda row: row.spent_money)
-    # Partitions are publicly known: 7 days.
-    public_partitions = list(range(1, 8))
+        partition_extractor=lambda mv: mv.movie_id,
+        privacy_id_extractor=lambda mv: mv.user_id,
+        value_extractor=lambda mv: mv.rating)
+    # There is no support for private partition selection without specifying
+    # max_partitions_contributed therefore we treat partitions as public.
+    public_partitions = common_utils.get_partitions(
+        data_extractors.partition_extractor, movie_views)
 
     # LocalBackend is more efficient for small datasets.
     backend = pipeline_dp.LocalBackend()
@@ -69,11 +52,11 @@ def main(unused_argv):
     # 0.9 on aggregation and 0.1 on calculation. Delta is not needed for DP
     # bounds calculations, therefore we don't split it.
     dp_tuning_params = pipeline_dp.CalculatePrivateContributionBoundsParams(
-        aggregation_noise_kind=pipeline_dp.NoiseKind.LAPLACE,
+        aggregation_noise_kind=pipeline_dp.NoiseKind.GAUSSIAN,
         aggregation_eps=0.9,
         aggregation_delta=1e-6,
         calculation_eps=0.1,
-        max_partitions_contributed_upper_bound=7)
+        max_partitions_contributed_upper_bound=100)
 
     # Define the privacy budget available for our computation. It will be used
     # only for aggregations, therefore total_epsilon here is aggregation_eps
@@ -86,8 +69,9 @@ def main(unused_argv):
 
     # Create a computational graph for the calculation.
     # All computations are lazy.
-    private_contribution_bounds_col = dp_engine.calculate_private_contribution_bounds(
-        restaraunt_visits_rows, dp_tuning_params, data_extractors,
+    private_contribution_bounds_col = \
+        dp_engine.calculate_private_contribution_bounds(
+        movie_views, dp_tuning_params, data_extractors,
         public_partitions)
     # Here's where the lazy iterator initiates computations and gets transformed
     # into actual results.
@@ -100,24 +84,45 @@ def main(unused_argv):
 
     params = pipeline_dp.AggregateParams(
         noise_kind=dp_tuning_params.aggregation_noise_kind,
-        # only COUNT and PRIVACY_ID_COUNT are supported for as of April 2023.
+        # only COUNT and PRIVACY_ID_COUNT are supported for now/
         metrics=[
-            pipeline_dp.Metrics.COUNT, pipeline_dp.Metrics.PRIVACY_ID_COUNT
+            # we can compute multiple metrics at once.
+            # For DP computation of contribution bounds only COUNT and
+            # PRIVACY_ID_COUNT is support as of April 2023.
+            pipeline_dp.Metrics.COUNT,
+            pipeline_dp.Metrics.PRIVACY_ID_COUNT,
         ],
+        # Limits to how much one user can contribute:
+        # .. at most two movies rated per user
+        # we estimated the best value for it in a dp way above.
         max_partitions_contributed=private_contribution_bounds.
         max_partitions_contributed,
-        # max_contributions_per_partition are not yet implemented.
-        max_contributions_per_partition=2)
+        # .. at most one rating for each movie
+        # DP estimation for max_contributions_per_partition is not yet
+        # supported.
+        max_contributions_per_partition=1)
 
-    # dp_result is iterable, but iterating it would fail until budget is
-    # computed (below).
+    # Create the Explain Computation report object for passing it into
+    # DPEngine.aggregate().
+    explain_computation_report = pipeline_dp.ExplainComputationReport()
+
+    # Create a computational graph for the aggregation.
+    # All computations are lazy. dp_result is iterable, but iterating it would
+    # fail until budget is computed (below).
     # It’s possible to call DPEngine.aggregate multiple times with different
     # metrics to compute.
-    dp_result = dp_engine.aggregate(restaraunt_visits_rows, params,
-                                    data_extractors, public_partitions)
+    dp_result = dp_engine.aggregate(movie_views, params, data_extractors,
+                                    public_partitions,
+                                    explain_computation_report)
 
     budget_accountant.compute_budgets()
 
+    # Generate the Explain Computation report. It must be called after
+    # budget_accountant.compute_budgets().
+    print(explain_computation_report.text())
+
+    # Here's where the lazy iterator initiates computations and gets transformed
+    # into actual results
     dp_result = list(dp_result)
 
     # Save the results
@@ -127,5 +132,6 @@ def main(unused_argv):
 
 
 if __name__ == '__main__':
+    flags.mark_flag_as_required("input_file")
     flags.mark_flag_as_required("output_file")
     app.run(main)
