994c759588506f658ab7e4d86aa19a8cdb84d4da
Vadym Doroshenko <dvadym@google.com>
2023-06-12 15:43:34 +0200

    wip

--- pipeline_dp/dataset_histograms/histogram_error_estimator.py
+++ pipeline_dp/dataset_histograms/histogram_error_estimator.py
@@ -14,17 +14,24 @@
 """TODO."""
 import pipeline_dp
 from pipeline_dp.dataset_histograms import histograms as hist
+
+
 class CountErrorEstimator:
-  def __init__(self, histograms: hist.DatasetHistograms, base_std:float, metric: pipeline_dp.Metrics):
-    # TODO: assert COUNT, PRIVACY_ID_COUNT
-    self._l0_ratios_dropped = hist.compute_ratio_dropped(histograms.l0_contributions_histogram)
-    self._linf_ratios_dropped = hist.compute_ratio_dropped(histograms.linf_contributions_histogram)
-    self._histograms = histograms
-    self._base_std = base_std
 
-  def estimate_rmse(self, l0: int, linf: int)->float:
-    ratio_dropped_l0 = self._get_ratio_dropped(self._l0_ratios_dropped, l0)
-    ratio_dropped_linf= 0
+    def __init__(self, histograms: hist.DatasetHistograms, base_std: float,
+                 metric: pipeline_dp.Metrics):
+        # TODO: assert COUNT, PRIVACY_ID_COUNT
+        self._l0_ratios_dropped = hist.compute_ratio_dropped(
+            histograms.l0_contributions_histogram)
+        self._linf_ratios_dropped = hist.compute_ratio_dropped(
+            histograms.linf_contributions_histogram)
+        self._histograms = histograms
+        self._base_std = base_std
 
+    def estimate_rmse(self, l0: int, linf: int) -> float:
+        ratio_dropped_l0 = self._get_ratio_dropped(self._l0_ratios_dropped, l0)
+        ratio_dropped_linf = 0
 
-  def _get_ratio_dropped(self, ratios_dropped:Sequence[Tuple[int, float]], bound:int):
+    def _get_ratio_dropped(self, ratios_dropped: Sequence[Tuple[int, float]],
+                           bound: int):
+        pass
