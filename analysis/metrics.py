6bab5633bb4b7e638aae8dd19e63b238b12923ac
Vadym Doroshenko <dvadym@google.com>
2023-06-14 14:34:53 +0200

    Minor renaming

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -250,12 +250,15 @@ class UtilityReport:
 class UtilityReportBin:
     """Stores a bin for a histogram of UtilityReports by partition size.
 
+    The partition size is the non-DP value of the metric whose utility analysis
+    was computed. The metric can be COUNT, PRIVACY_ID_COUNT, SUM.
+
      Attributes:
-        size_from: lower bound of partitions size.
-        size_to: upper bound of partitions size.
+        partition_sizefrom: lower bound of partitions size.
+        partition_sizeto: upper bound of partitions size.
         report: the result of utility analysis for partitions of size
-          [size_from, size_to).
+          [partition_sizefrom, partition_sizeto).
     """
-    size_from: int
-    size_to: int
+    partition_sizefrom: int
+    partition_sizeto: int
     report: UtilityReport
