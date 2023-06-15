d64795a9d4a46dc9ffa771affa9e3203c5600d34
RamSaw <mpravilov@google.com>
2023-06-15 15:27:16 +0200

    Remove unused imports.

--- analysis/parameter_tuning.py
+++ analysis/parameter_tuning.py
@@ -12,11 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import math
-import typing
 
 import pipeline_dp
 from pipeline_dp import pipeline_backend
-from functools import partial
 from pipeline_dp import input_validators
 from pipeline_dp.dataset_histograms import histograms
 import analysis
