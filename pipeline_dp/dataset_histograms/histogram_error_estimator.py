29210302ad07f61ab4b3f5c6a5631f4d51394ea1
Vadym Doroshenko <dvadym@google.com>
2023-06-16 11:54:54 +0200

    wip

--- pipeline_dp/dataset_histograms/histogram_error_estimator.py
+++ pipeline_dp/dataset_histograms/histogram_error_estimator.py
@@ -14,12 +14,14 @@
 """TODO."""
 import pipeline_dp
 from pipeline_dp.dataset_histograms import histograms as hist
+from typing import Optional, Sequence, Tuple
+import math
 
 
 class CountErrorEstimator:
 
     def __init__(self, histograms: hist.DatasetHistograms, base_std: float,
-                 metric: pipeline_dp.Metrics):
+                 metric: pipeline_dp.Metrics, noise: pipeline_dp.NoiseKind):
         # TODO: assert COUNT, PRIVACY_ID_COUNT
         self._l0_ratios_dropped = hist.compute_ratio_dropped(
             histograms.l0_contributions_histogram)
@@ -27,11 +29,32 @@ class CountErrorEstimator:
             histograms.linf_contributions_histogram)
         self._histograms = histograms
         self._base_std = base_std
+        self._metric = metric
+        self._num_partitions = histograms.count_per_partition_histogram.total_count(
+        )
 
-    def estimate_rmse(self, l0: int, linf: int) -> float:
+    def estimate_rmse(self, l0: int, linf: Optional[int] = None) -> float:
+        # todo validate linf
         ratio_dropped_l0 = self._get_ratio_dropped(self._l0_ratios_dropped, l0)
         ratio_dropped_linf = 0
+        if self._metric == pipeline_dp.Metrics.COUNT:
+            ratio_dropped_linf = self._get_ratio_dropped(
+                self._linf_ratios_dropped, linf)
+            partition_histogram = self._histograms.count_per_partition_histogram
+        else:
+            partition_histogram = self._histograms.count_privacy_id_per_partition
+        ratio_dropped = 1 - (1 - ratio_dropped_l0) * (1 - ratio_dropped_linf)
+        sigma = self._get_sigma(l0, linf)
+        sum_rmse = 0
+        for bin in partition_histogram.bins:
+            average_per_bin = bin.sum / bin.count
+            rmse = math.sqrt((ratio_dropped * average_per_bin)**2 + sigma**2)
+            sum_rmse += bin.count * rmse
+        return sum_rmse / self._num_partitions
 
     def _get_ratio_dropped(self, ratios_dropped: Sequence[Tuple[int, float]],
-                           bound: int):
+                           bound: int) -> float:
+        pass
+
+    def _get_sigma(self, l0: int, linf: Optional[int] = None) -> float:
         pass
