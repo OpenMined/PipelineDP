b9cc62ad6536604ae39d73c387a88dada9597522
Vadym Doroshenko <dvadym@google.com>
2023-08-25 13:46:50 +0200

    wip

--- analysis/dp_algorithm_selector.py
+++ analysis/dp_algorithm_selector.py
@@ -1,6 +1,6 @@
 import pipeline_dp
 from dataclasses import dataclass
-
+from typing import List
 
 @dataclass
 class DPStrategy:
@@ -11,8 +11,17 @@ class DPStrategy:
 
 class DPAlgorithmSelector:
 
-    def __init__(self, metric: pipeline_dp.Metric):
+    def __init__(self, metric: pipeline_dp.Metric, is_public_partitions:bool):
         self._metric = metric
+        self._is_public_partitions = is_public_partitions
 
     def get_dp_strategy(self, l0: int, linf: float) -> DPStrategy:
         pass
+
+    def does_support_return_thresholding_values(self):
+        return True
+
+    def supported_noise_kind(self)->List[pipeline_dp.NoiseKind]:
+        return [pipeline_dp.NoiseKind.LAPLACE, pipeline_dp.NoiseKind.GAUSSIAN]
+
+    # def supported_
