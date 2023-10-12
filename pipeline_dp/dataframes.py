954be903a3da8755fd0e0bcf2e81d08c96bc0e37
Vadym Doroshenko <dvadym@google.com>
2023-10-12 17:00:59 +0200

    DataFrame query builder

--- pipeline_dp/dataframes.py
+++ pipeline_dp/dataframes.py
@@ -14,7 +14,7 @@
 """Computing DP aggregations on (Pandas, Spark, Beam) Dataframes."""
 
 from dataclasses import dataclass
-from typing import Optional, List, Dict, Iterable
+from typing import Any, Dict, Iterable, List, Optional
 
 import pipeline_dp
 
@@ -47,16 +47,19 @@ class Budget:
 
 
 class Query:
+    """DP Query on DataFrames.
+
+    Warning: Don't create it directly, use QueryBuilder().
+    """
 
     def __init__(self, df, columns: Columns, metrics: Dict[pipeline_dp.Metric,
                                                            List[str]],
-                 contribution_bonds: ContributionBounds, public_keys):
+                 contribution_bounds: ContributionBounds, public_keys):
         self._df = df
         self._columns = columns
         self._metrics = metrics
-        self._contribution_bonds = contribution_bonds
+        self._contribution_bounds = contribution_bounds
         self._public_partitions = public_keys
-        self._expain_computation_report = None
 
     def run_query(self,
                   budget: Budget,
@@ -65,13 +68,35 @@ class Query:
 
 
 class QueryBuilder:
+    """Builds DP queries on (Spark, Pandas, Beam) DataFrames.
 
-    def __init__(self, df, privacy_key_column: str):
-        if privacy_key_column not in df.scheme:
+    It uses a Builder pattern, each public function except of build_query
+    returns the reference to itself.
+    The usage is
+    QueryBuilder().groupby(...).aggregation1(...)....aggregation_n().build_query()
+
+    Example:
+        # The following query computes restaurant visits and total money
+        # spent:
+        query = QueryBuilder(df, "user_id")
+        .groupby("day", max_groups_contributed=3, max_contributions_per_group=1)
+        .count()
+        .sum("money_spent", min_value=0, max_value=100)
+        .build_query()
+    """
+
+    def __init__(self, df, privacy_unit_column: str):
+        """Constructor.
+
+        Args:
+            df: (Spark, Pandas, Beam) DataFrame with data to anonymize.
+            privacy_key_column: column from `df` with privacy key.
+        """
+        if privacy_unit_column not in df.columns:
             raise ValueError(
-                f"Column {privacy_key_column} is not present in DataFrame")
+                f"Column {privacy_unit_column} is not present in DataFrame")
         self._df = df
-        self._privacy_key_column = privacy_key_column
+        self._privacy_unit_column = privacy_unit_column
         self._groupby_column = None
         self._value_column = None
         self._metrics = {}  # map from pipeline_dp.Metric -> output column name
@@ -81,18 +106,41 @@ class QueryBuilder:
     def groupby(self,
                 column: str,
                 *,
-                max_partitions_contributed: int,
-                max_contributions_per_partition: int,
-                public_keys=None) -> 'QueryBuilder':
+                max_groups_contributed: int,
+                max_contributions_per_group: int,
+                public_keys=Iterable[Any]) -> 'QueryBuilder':
+        """Adds groupby by the given column to query.
+
+        All following aggregation will be applied to grouped by DataFrame.
+
+        Args:
+            column: column to group.
+            max_groups_contributed: the maximum groups that can each privacy
+              unit contributes to the result. If some privacy unit contributes
+              more in the input dataset, the groups are sub-sampled to
+              max_groups_contributed.
+            max_contributions_per_group: the maximum contribution that a privacy
+              unit can contribute to a group. If some privacy unit contributes
+              more to some group, contributions are sub-sampled to
+              max_contributions_per_group.
+            public_keys:
+        """
         if self._groupby_column is not None:
             raise ValueError("groupby can be called only once.")
+        if column not in self._df.columns:
+            raise ValueError(f"Column {column} is not present in DataFrame")
         self._groupby_column = column
-        self._contribution_bounds.max_partitions_contributed = max_partitions_contributed
-        self._contribution_bounds.max_contributions_per_partition = max_contributions_per_partition
+        self._contribution_bounds.max_partitions_contributed = max_groups_contributed
+        self._contribution_bounds.max_contributions_per_partition = max_contributions_per_group
         self._public_keys = public_keys
         return self
 
     def count(self, name: str = None) -> 'QueryBuilder':
+        """Adds count to the query.
+
+        Args:
+            name: the name output column.
+        """
         if self._groupby_column is None:
             raise ValueError(
                 "Global aggregations are not supported. Use groupby.")
@@ -107,11 +155,21 @@ class QueryBuilder:
             min_value: float,
             max_value: float,
             name: str = None) -> 'QueryBuilder':
+        """Adds sum to the query.
+
+        Args:
+            column: column to perform summation
+            min_value, max_value: capping limits to each value.
+            name: the name output column.
+        """
         if self._groupby_column is None:
             raise ValueError(
                 "Global aggregations are not supported. Use groupby.")
         if pipeline_dp.Metrics.SUM in self._metrics:
             raise ValueError("sum can be counted only once.")
+        if self._value_column is not None:
+            raise NotImplementedError(
+                "Now aggregation of only 1 column is supported.")
         self._metrics[pipeline_dp.Metrics.SUM] = name
         self._value_column = column
         self._contribution_bounds.min_value = min_value
@@ -119,12 +177,13 @@ class QueryBuilder:
         return self
 
     def build_query(self) -> Query:
+        """Builds the DP query."""
         if self._groupby_column is None:
             raise NotImplementedError(
                 "Global aggregations are not implemented yet. Call groupby.")
         metrics = list(self._metrics.keys())
         return Query(
             self._df,
-            Columns(self._privacy_key_column, self._groupby_column,
+            Columns(self._privacy_unit_column, self._groupby_column,
                     self._value_column), metrics, self._contribution_bounds,
             self._public_keys)
--- tests/dataframes_test.py
+++ tests/dataframes_test.py
@@ -11,3 +11,40 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for DP aggregations on DataFrames."""
+from absl.testing import absltest
+from absl.testing import parameterized
+
+import pandas as pd
+import pipeline_dp
+from pipeline_dp import dataframes
+
+
+class QueryBuilderTest(parameterized.TestCase):
+
+    def _get_pandas_df(self) -> pd.DataFrame:
+        return pd.DataFrame({
+            "privacy_key": [0, 1, 1],
+            "partition_key": ["key1", "key2", "key1"],
+            "value": [5.0, 2.5, -2]
+        })
+
+    def test_query_count(self):
+        df = self._get_pandas_df()
+        query = dataframes.QueryBuilder(df, "privacy_key").groupby(
+            "partition_key",
+            max_groups_contributed=5,
+            max_contributions_per_group=10).count().build_query()
+        self.assertTrue(query._df.equals(df))
+        self.assertEqual(
+            query._columns,
+            dataframes.Columns("privacy_key", "partition_key", None))
+        self.assertEqual(query._metrics, [pipeline_dp.Metrics.COUNT])
+        self.assertEqual(
+            query._contribution_bounds,
+            dataframes.ContributionBounds(max_partitions_contributed=5,
+                                          max_contributions_per_partition=10))
+
+
+if __name__ == '__main__':
+    absltest.main()
