6ddb38adb1e52cfd33a995ecfa0681b58ef30b4d
Vadym Doroshenko <dvadym@google.com>
2023-10-30 16:16:57 +0100

    wip

--- analysis/dp_algorithm_selector.py
+++ analysis/dp_algorithm_selector.py
@@ -19,19 +19,27 @@ from typing import List, Optional
 import pipeline_dp
 from pydp.algorithms import numerical_mechanisms as dp_mechanisms
 
+
 @dataclass
 class DPStrategy:
     noise_kind: pipeline_dp.NoiseKind
-    partition_selection_strategy: Optional[pipeline_dp.PartitionSelectionStrategy]
+    partition_selection_strategy: Optional[
+        pipeline_dp.PartitionSelectionStrategy]
     post_aggregation_thresholding: bool
 
 
 class AvailableMethods:
-    def supported_noise_kind(self)->List[pipeline_dp.NoiseKind]:
+
+    def supported_noise_kind(self) -> List[pipeline_dp.NoiseKind]:
         return [pipeline_dp.NoiseKind.LAPLACE, pipeline_dp.NoiseKind.GAUSSIAN]
 
-    def supported_partition_selection_strategies(self)->List[pipeline_dp.PartitionSelectionStrategy]:
-        return [pipeline_dp.PartitionSelectionStrategy.TRUNCATED_GEOMETRIC, pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING, pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING]
+    def supported_partition_selection_strategies(
+            self) -> List[pipeline_dp.PartitionSelectionStrategy]:
+        return [
+            pipeline_dp.PartitionSelectionStrategy.TRUNCATED_GEOMETRIC,
+            pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING,
+            pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING
+        ]
 
     def support_post_aggregation_thresholding(self):
         return True
@@ -39,7 +47,9 @@ class AvailableMethods:
 
 class DPAlgorithmSelector:
 
-    def __init__(self, epsilon:float, delta:float, metric: pipeline_dp.Metric, is_public_partitions:bool, available_methods:AvailableMethods):
+    def __init__(self, epsilon: float, delta: float, metric: pipeline_dp.Metric,
+                 is_public_partitions: bool,
+                 available_methods: AvailableMethods):
         self._epsilon = epsilon
         self._delta = delta
         self._metric = metric
@@ -50,25 +60,36 @@ class DPAlgorithmSelector:
         # todo:validate l0, linf
         if self._is_public_partitions:
             return self._get_dp_strategy_for_public_partitions(l0, linf)
-        if self._metric == pipeline_dp.Metrics.PRIVACY_ID_COUNT and self._available_methods.support_post_aggregation_thresholding():
+        if self._metric == pipeline_dp.Metrics.PRIVACY_ID_COUNT and self._available_methods.support_post_aggregation_thresholding(
+        ):
             return self._get_dp_strategy_with_post_aggregation_threshold(l0)
-    def _get_dp_strategy_for_public_partitions(self, l0: int, linf: float) -> DPStrategy:
-        gaussian_std = self._get_gaussian_std(self._epsilon, self._delta, l0, linf)
+
+    def _get_dp_strategy_for_public_partitions(self, l0: int,
+                                               linf: float) -> DPStrategy:
+        gaussian_std = self._get_gaussian_std(self._epsilon, self._delta, l0,
+                                              linf)
         laplace_std = self._get_gaussian_std(self._epsilon, l0, linf)
         if gaussian_std < laplace_std:
             noise_kind = pipeline_dp.NoiseKind.GAUSSIAN
         else:
             noise_kind = pipeline_dp.NoiseKind.LAPLACE
-        return DPStrategy(noise_kind=noise_kind, partition_selection_strategy=None, post_aggregation_thresholding=False)
+        return DPStrategy(noise_kind=noise_kind,
+                          partition_selection_strategy=None,
+                          post_aggregation_thresholding=False)
 
-    def _get_dp_strategy_with_post_aggregation_threshold(self, l0: int) -> DPStrategy:
+    def _get_dp_strategy_with_post_aggregation_threshold(self,
+                                                         l0: int) -> DPStrategy:
         pass
 
     # def _choose_noise_kind(self, epsilon:float, delta:float)->pipeline_dp.NoiseKind:
-    def _get_gaussian_std(self, epsilon:float, delta:float,  l0: int, linf: float)-> float:
-        l2_sensitivity = math.sqrt(l0)*linf
-        return dp_mechanisms.GaussianMechanism(epsilon, delta, sensitivity=l2_sensitivity).std
+    def _get_gaussian_std(self, epsilon: float, delta: float, l0: int,
+                          linf: float) -> float:
+        l2_sensitivity = math.sqrt(l0) * linf
+        return dp_mechanisms.GaussianMechanism(epsilon,
+                                               delta,
+                                               sensitivity=l2_sensitivity).std
 
-    def _get_laplace_std(self, epsilon:float, l0: int, linf: float)-> float:
-        l1_sensitivity = l0*linf
-        return dp_mechanisms.GaussianMechanism(epsilon, sensitivity=l1_sensitivity).std
+    def _get_laplace_std(self, epsilon: float, l0: int, linf: float) -> float:
+        l1_sensitivity = l0 * linf
+        return dp_mechanisms.GaussianMechanism(epsilon,
+                                               sensitivity=l1_sensitivity).std
