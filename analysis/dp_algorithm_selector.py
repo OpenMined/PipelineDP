ba26bc055cede6af11324e607c2578355ae28e43
Vadym Doroshenko <dvadym@google.com>
2023-10-31 11:06:46 +0100

    wip

--- analysis/dp_algorithm_selector.py
+++ analysis/dp_algorithm_selector.py
@@ -17,6 +17,7 @@ from dataclasses import dataclass
 from typing import List, Optional
 
 import pipeline_dp
+from pipeline_dp import aggregate_params
 from pipeline_dp import dp_computations
 from pydp.algorithms import numerical_mechanisms as dp_mechanisms
 
@@ -28,82 +29,43 @@ class DPStrategy:
         pipeline_dp.PartitionSelectionStrategy]
     post_aggregation_thresholding: bool
 
-
-class AvailableMethods:
-
-    def supported_noise_kind(self) -> List[pipeline_dp.NoiseKind]:
-        return [pipeline_dp.NoiseKind.LAPLACE, pipeline_dp.NoiseKind.GAUSSIAN]
-
-    def supported_partition_selection_strategies(
-            self) -> List[pipeline_dp.PartitionSelectionStrategy]:
-        return [
-            pipeline_dp.PartitionSelectionStrategy.TRUNCATED_GEOMETRIC,
-            pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING,
-            pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING
-        ]
-
-    def support_post_aggregation_thresholding(self):
-        return True
+    def __post_init__(self):
+        pass  # todo
 
 
-class DPAlgorithmSelector:
+class DPStrategySelector:
 
     def __init__(self, epsilon: float, delta: float, metric: pipeline_dp.Metric,
-                 is_public_partitions: bool, pre_threshold: Optional[int],
-                 available_methods: AvailableMethods):
+                 is_public_partitions: bool, pre_threshold: Optional[int]):
         self._epsilon = epsilon
         self._delta = delta
         self._metric = metric
         self._is_public_partitions = is_public_partitions
         self._pre_threshold = pre_threshold
-        self._available_methods = available_methods
 
     def get_dp_strategy(self, l0: int, linf: float) -> DPStrategy:
         # todo:validate l0, linf
+        # todo: should be used Sensitivities ?
         if self._is_public_partitions:
             return self._get_dp_strategy_for_public_partitions(l0, linf)
-        if self._metric == pipeline_dp.Metrics.PRIVACY_ID_COUNT and self._available_methods.support_post_aggregation_thresholding(
-        ):
+        if self.use_post_aggregation_thresholding():
             return self._get_dp_strategy_with_post_aggregation_threshold(l0)
         return self._get_dp_strategy_private_partition(l0, linf)
 
     def _get_dp_strategy_for_public_partitions(self, l0: int,
                                                linf: float) -> DPStrategy:
-        supported_noise_kinds = self._available_methods.supported_noise_kind()
-        if len(supported_noise_kinds) == 1:
-            return DPStrategy(noise_kind=supported_noise_kinds[0],
-                              partition_selection_strategy=None,
-                              post_aggregation_thresholding=False)
-        gaussian_std = self._get_gaussian_std(self._epsilon, self._delta, l0,
-                                              linf)
-        laplace_std = self._get_gaussian_std(self._epsilon, l0, linf)
-        if gaussian_std < laplace_std:
-            noise_kind = pipeline_dp.NoiseKind.GAUSSIAN
-        else:
-            noise_kind = pipeline_dp.NoiseKind.LAPLACE
+        noise_kind = self.choose_noise_kind(self._epsilon, self._delta, l0,
+                                            linf)
         return DPStrategy(noise_kind=noise_kind,
                           partition_selection_strategy=None,
                           post_aggregation_thresholding=False)
 
     def _get_dp_strategy_with_post_aggregation_threshold(self,
                                                          l0: int) -> DPStrategy:
-
-        def create_mechanism(strategy: pipeline_dp.PartitionSelectionStrategy):
-            return dp_computations.ThresholdingMechanism(
-                self._epsilon, self._delta, strategy, l0, self._pre_threshold)
-
-        gaussian_thresholding_mechanism = create_mechanism(
-            pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING)
-        laplace_thresholding_mechanism = create_mechanism(
-            pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING)
-        # todo: if supported
-        if gaussian_thresholding_mechanism.noise_std(
-        ) < laplace_thresholding_mechanism.noise_std():
-            noise_kind = pipeline_dp.NoiseKind.GAUSSIAN
-            partition_selection_strategy = pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING
-        else:
-            noise_kind = pipeline_dp.NoiseKind.LAPLACE
-            partition_selection_strategy = pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING
+        delta_noise = self._delta / 2  # todo comment
+        noise_kind = self.choose_noise_kind(self._epsilon, delta_noise, l0, 1)
+        partition_selection_strategy = aggregate_params.noise_to_thresholding(
+            noise_kind)
         return DPStrategy(
             noise_kind=noise_kind,
             partition_selection_strategy=partition_selection_strategy,
@@ -111,7 +73,14 @@ class DPAlgorithmSelector:
 
     def _get_dp_strategy_private_partition(self, l0: int,
                                            linf: float) -> DPStrategy:
-        pass
+        half_epsilon, half_delta = self._epsilon / 2, self._delta / 2
+        noise_kind = self.choose_noise_kind(half_epsilon, half_delta, l0, linf)
+        partition_selection_strategy = self.choose_partition_selection(
+            half_epsilon, half_delta, l0)
+        return DPStrategy(
+            noise_kind=noise_kind,
+            partition_selection_strategy=partition_selection_strategy,
+            post_aggregation_thresholding=False)
 
     # def _choose_noise_kind(self, epsilon:float, delta:float)->pipeline_dp.NoiseKind:
     def _get_gaussian_std(self, epsilon: float, delta: float, l0: int,
@@ -123,5 +92,35 @@ class DPAlgorithmSelector:
 
     def _get_laplace_std(self, epsilon: float, l0: int, linf: float) -> float:
         l1_sensitivity = l0 * linf
-        return dp_mechanisms.GaussianMechanism(epsilon,
-                                               sensitivity=l1_sensitivity).std
+        return dp_mechanisms.LaplaceMechanism(epsilon,
+                                              sensitivity=l1_sensitivity).std
+
+    def choose_noise_kind(self, epsilon: float, delta: float, l0: int,
+                          linf: float) -> pipeline_dp.NoiseKind:
+        gaussian_std = self._get_gaussian_std(epsilon, delta, l0, linf)
+        laplace_std = self._get_gaussian_std(epsilon, l0, linf)
+        if gaussian_std < laplace_std:
+            return pipeline_dp.NoiseKind.GAUSSIAN
+        return pipeline_dp.NoiseKind.LAPLACE
+
+    def use_post_aggregation_thresholding(self,
+                                          metric: pipeline_dp.Metric) -> bool:
+        return metric == pipeline_dp.Metrics.PRIVACY_ID_COUNT
+
+    def choose_partition_selection(
+            self, epsilon: float, delta: float,
+            l0: int) -> pipeline_dp.PartitionSelectionStrategy:
+
+        def create_mechanism(strategy: pipeline_dp.PartitionSelectionStrategy):
+            return dp_computations.ThresholdingMechanism(
+                epsilon, delta, strategy, l0, self._pre_threshold)
+
+        laplace_thresholding_mechanism = create_mechanism(
+            pipeline_dp.PartitionSelectionStrategy.LAPLACE_THRESHOLDING)
+        gaussian_thresholding_mechanism = create_mechanism(
+            pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING)
+        if laplace_thresholding_mechanism.threshold(
+        ) < gaussian_thresholding_mechanism.threshold():
+            # todo: explain why
+            return pipeline_dp.PartitionSelectionStrategy.TRUNCATED_GEOMETRIC
+        return pipeline_dp.PartitionSelectionStrategy.GAUSSIAN_THRESHOLDING
--- pipeline_dp/dp_computations.py
+++ pipeline_dp/dp_computations.py
@@ -810,8 +810,8 @@ class ThresholdingMechanism:
         # TODO: add noise scale to text, when it's exposed from C++.
         return text
 
-    def noise_std(self) -> float:
-        return self._thresholding_strategy.noise_std()
+    def threshold(self) -> float:
+        return self._thresholding_strategy.threshold()
 
 
 def create_thresholding_mechanism(
