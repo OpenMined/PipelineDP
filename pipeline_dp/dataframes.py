f1744610fd6fe9fe6ae212ce4c24652f545664c2
Vadym Doroshenko <53558779+dvadym@users.noreply.github.com>
2023-11-20 14:55:44 +0100

    Implement mean in DataFrame API (#505)

--- pipeline_dp/dataframes.py
+++ pipeline_dp/dataframes.py
@@ -15,7 +15,7 @@
 import abc
 from collections import namedtuple
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Iterable, Optional, Sequence, Union
+from typing import Any, Dict, Iterable, List, Optional, Sequence, Tuple, Union
 
 import pipeline_dp
 import pyspark
@@ -220,6 +220,23 @@ class Query:
         return converter.collection_to_dataframe(dp_result)
 
 
+@dataclass
+class _AggregationSpec:
+    """Contains a specification of one aggregations (e.g. count, mean)
+
+    Attributes:
+          metric: which aggregation to perform.
+          input_column: the name of the column to perform aggregation.
+          output_column: the name of the output column.
+          min_value, max_value: capping limits to each value.
+    """
+    metric: pipeline_dp.Metric
+    input_column: Optional[str]
+    output_column: Optional[str]
+    min_value: Optional[float] = None
+    max_value: Optional[float] = None
+
+
 class QueryBuilder:
     """Builds DP queries on (Spark, Pandas, Beam) DataFrames.
 
@@ -234,7 +251,9 @@ class QueryBuilder:
         query = QueryBuilder(df, "user_id")
         .groupby("day", max_groups_contributed=3, max_contributions_per_group=1)
         .count()
-        .sum("money_spent", min_value=0, max_value=100)
+        .sum("spent_money", min_value=0, max_value=100)
+        .mean("spent_money") # no need to specify min_value, max_value, they
+        # were already given in sum
         .build_query()
     """
 
@@ -251,10 +270,10 @@ class QueryBuilder:
         self._df = df
         self._privacy_unit_column = privacy_unit_column
         self._by = None
-        self._value_column = None
-        self._metrics = {}  # map from pipeline_dp.Metric -> output column name
-        self._contribution_bounds = ContributionBounds()
         self._public_keys = None
+        self._aggregations_specs = []
+        self._max_partitions_contributed: int = None
+        self._max_contributions_per_partition: int = None
 
     def groupby(
             self,
@@ -269,6 +288,12 @@ class QueryBuilder:
 
         All following aggregation will be applied to grouped by DataFrame.
 
+        Note:  If 'public_keys' are provided, then the keys of the output
+          DataFrame keys  will exactly coincide with provided 'public_keys'.
+          If some public key is missing in the input DataFrame the output
+          DataFrame will still a value, to ensure DP. For example in case if
+          COUNT, the value will be 0 + noise.
+
         Args:
             by: a column or a list of columns used to determine the groups.
             max_groups_contributed: the maximum groups that can each privacy
@@ -279,12 +304,14 @@ class QueryBuilder:
               unit can contribute to a group. If some privacy unit contributes
               more to some group, contributions are sub-sampled to
               max_contributions_per_group.
-            public_keys:
+            public_keys: the list of groupby keys which are known before the
+              running aggregations. It can be for example dates, countries etc.
+              If not provided, keys will be selected in a DP manner.
         """
         if column is not None:
             raise ValueError("column argument is deprecated. Use by")
         if self._by is not None:
-            raise ValueError("groupby can be called only once.")
+            raise ValueError("groupby can be called only once")
 
         if isinstance(by, str):
             if by not in self._df.columns:
@@ -298,8 +325,8 @@ class QueryBuilder:
         else:
             raise ValueError(f"by argument must be column name(s)")
         self._by = by
-        self._contribution_bounds.max_partitions_contributed = max_groups_contributed
-        self._contribution_bounds.max_contributions_per_partition = max_contributions_per_group
+        self._max_partitions_contributed = max_groups_contributed
+        self._max_contributions_per_partition = max_contributions_per_group
         self._public_keys = public_keys
         return self
 
@@ -309,50 +336,136 @@ class QueryBuilder:
         Args:
             name: the name of the output column.
         """
-        if self._by is None:
-            raise ValueError(
-                "Global aggregations are not supported. Use groupby.")
-        if pipeline_dp.Metrics.COUNT in self._metrics:
-            raise ValueError("count can be counted only once.")
-        self._metrics[pipeline_dp.Metrics.COUNT] = name
-        return self
+        return self._add_aggregation(
+            _AggregationSpec(metric=pipeline_dp.Metrics.COUNT,
+                             input_column=None,
+                             output_column=name))
 
     def sum(self,
             column: str,
             *,
-            min_value: float,
-            max_value: float,
+            min_value: Optional[float] = None,
+            max_value: Optional[float] = None,
             name: str = None) -> 'QueryBuilder':
-        """Adds sum to the query.
+        """Adds sum aggregation to the query.
 
         Args:
             column: column to perform summation
             min_value, max_value: capping limits to each value.
             name: the name of the output column.
         """
-        if self._by is None:
-            raise ValueError(
-                "Global aggregations are not supported. Use groupby.")
-        if pipeline_dp.Metrics.SUM in self._metrics:
-            raise ValueError("sum can be counted only once.")
-        if self._value_column is not None:
-            raise NotImplementedError(
-                "Now aggregation of only 1 column is supported.")
-        self._metrics[pipeline_dp.Metrics.SUM] = name
-        self._value_column = column
-        self._contribution_bounds.min_value = min_value
-        self._contribution_bounds.max_value = max_value
-        return self
+        return self._add_aggregation(
+            _AggregationSpec(metric=pipeline_dp.Metrics.SUM,
+                             input_column=column,
+                             output_column=name,
+                             min_value=min_value,
+                             max_value=max_value))
+
+    def mean(self,
+             column: str,
+             *,
+             min_value: Optional[float] = None,
+             max_value: Optional[float] = None,
+             name: str = None) -> 'QueryBuilder':
+        """Adds mean aggregation to the query.
+
+        Args:
+            column: column to compute mean
+            min_value, max_value: capping limits to each value.
+            name: the name of the output column.
+        """
+
+        return self._add_aggregation(
+            _AggregationSpec(metric=pipeline_dp.Metrics.MEAN,
+                             input_column=column,
+                             output_column=name,
+                             min_value=min_value,
+                             max_value=max_value))
 
     def build_query(self) -> Query:
         """Builds the DP query."""
+        self._check_by()
+        self._check_aggregations_specs()
+        self._check_metrics()
+
+        input_column = self._get_input_column()
+        min_value, max_value = self._get_value_caps()
+
+        # Create Contribution bounds
+        contribution_bounds = ContributionBounds(
+            max_partitions_contributed=self._max_partitions_contributed,
+            max_contributions_per_partition=self.
+            _max_contributions_per_partition,
+            min_value=min_value,
+            max_value=max_value)
+
+        metric_to_output_column = dict((spec.metric, spec.output_column)
+                                       for spec in self._aggregations_specs)
+
+        return Query(self._df,
+                     Columns(self._privacy_unit_column, self._by,
+                             input_column), metric_to_output_column,
+                     contribution_bounds, self._public_keys)
+
+    def _add_aggregation(self,
+                         aggregation_spec: _AggregationSpec) -> 'QueryBuilder':
+        self._check_by()
+        self._aggregations_specs.append(aggregation_spec)
+        return self
+
+    def _check_by(self) -> None:
         if self._by is None:
             raise NotImplementedError(
-                "Global aggregations are not implemented yet. Call groupby.")
-        if not self._metrics:
+                "Global aggregations are not implemented yet. Call groupby")
+
+    def _check_aggregations_specs(self) -> None:
+        if not self._aggregations_specs:
             raise ValueError(
-                "No aggregations in the query. Call for example count.")
-        return Query(
-            self._df,
-            Columns(self._privacy_unit_column, self._by, self._value_column),
-            self._metrics, self._contribution_bounds, self._public_keys)
+                "No aggregations in the query. Call count, sum, mean etc")
+
+    def _get_input_column(self) -> Optional[str]:
+        input_columns = [
+            spec.input_column
+            for spec in self._aggregations_specs
+            if spec.input_column is not None
+        ]
+        if len(set(input_columns)) > 1:
+            raise NotImplementedError(
+                f"Aggregation of only one column is supported, but {input_columns} given"
+            )
+        return input_columns[0] if input_columns else None
+
+    def _check_metrics(self) -> List[pipeline_dp.Metric]:
+        metrics = [spec.metric for spec in self._aggregations_specs]
+        if len(set(metrics)) != len(metrics):
+            raise ValueError("Each aggregation can be added only once.")
+        return metrics
+
+    def _get_value_caps(self) -> Tuple[Optional[float], Optional[float]]:
+        metrics = set([spec.metric for spec in self._aggregations_specs])
+        # COUNT, PPRIVACY_ID_COUNT do not require caps.
+        metrics_which_need_caps = metrics.difference(
+            [pipeline_dp.Metrics.COUNT, pipeline_dp.Metrics.PRIVACY_ID_COUNT])
+        if not metrics_which_need_caps:
+            return None, None
+        min_values = [
+            spec.min_value
+            for spec in self._aggregations_specs
+            if spec.min_value is not None
+        ]
+        max_values = [
+            spec.max_value
+            for spec in self._aggregations_specs
+            if spec.max_value is not None
+        ]
+        if not min_values or not max_values:
+            raise ValueError("min_value and max_value must be given at least "
+                             "once as arguments of sum or mean")
+
+        def all_values_equal(a: list) -> bool:
+            return min(a) == max(a)
+
+        if not all_values_equal(min_values) or not all_values_equal(max_values):
+            raise ValueError("If min_value and max_value provided multiple "
+                             "times they must be the same")
+        return min_values[0], max_values[0]
