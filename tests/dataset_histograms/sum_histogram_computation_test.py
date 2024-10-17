ceca7812704b7b38bd85d2db826faaae160f0d3d
Vadym Doroshenko <dvadym@google.com>
2024-10-17 13:17:52 +0200

    Move count hisgorams to a separate file

--- tests/dataset_histograms/sum_histogram_computation_test.py
+++ tests/dataset_histograms/sum_histogram_computation_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for computing dataset histograms."""
+"""Tests for computing sum dataset histograms."""
 
 from absl.testing import absltest
 from absl.testing import parameterized
