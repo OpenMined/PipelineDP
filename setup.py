1a5eb9701b392c34dfb195ee6c7e406e0eaa7b8f
Vadym Doroshenko <dvadym@google.com>
2026-05-11 12:49:44 +0200

    Bump version to 0.3.0 fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(fname):
 
 setup_kwargs = {
     'name': 'pipeline-dp',
-    'version': '0.3.0rc1',
+    'version': '0.3.0',
     'description': 'Framework for applying differential privacy to large datasets using batch processing systems',
     'author': 'Chinmay Shah',
     'author_email': 'chinmayshah3899@gmail.com',
