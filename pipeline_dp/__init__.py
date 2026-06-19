41d3f5b48f496449bf37bf5143d8d40b240aaca7
Vadym Doroshenko <dvadym@google.com>
2026-06-19 15:11:41 +0200

    fix

--- pipeline_dp/__init__.py
+++ pipeline_dp/__init__.py
@@ -48,7 +48,6 @@ __all__ = [
     'SelectPartitionsParams',
     'SumParams',
     'VarianceParams',
-    'QuantileParams',
     'CalculatePrivateContributionBoundsParams',
     'PrivateContributionBounds',
     'AddDPNoiseParams',
